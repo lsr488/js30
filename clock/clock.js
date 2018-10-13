3ed29530f1626ed69cfe42b94cf5dc6f4bac2e6c
Lisa <lsr488@gmail.com>
2018-10-13 14:43:23 -0400

    Added numbers to clock face.

--- clock/clock.js
+++ clock/clock.js
@@ -5,19 +5,24 @@ const hourHand = document.querySelector('.hour-hand');
 function setDate() {
 	const now = new Date();
 	const seconds = now.getSeconds();
-	const secondsDegrees = ((seconds / 60) * 360 + 90); // seconds/60 is the percentage of seconds around the clockface, 360 is the whole circle, 90 is the origin-offset;
+	const secondsDegrees = ((seconds / 60) * 360) + 90; // seconds/60 is the percentage of seconds around the clockface, 360 is the whole circle, 90 is the origin-offset;
 	secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
 
 	const minutes = now.getMinutes();
-	const minutesDegrees = ((minutes / 60) * 360 + 90);
+	const minutesDegrees = ((minutes / 60) * 360) + ((seconds/60)*6) + 90;
 	minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
 
-	const hours = now.getHours();
-	const hoursDegrees = ((hours / 60) * 360 + 90);
+	let hours = now.getHours();
+	// console.log(hours);
+	if(hours > 12) {
+		hours = hours - 12;
+	}
+	// console.log(hours);
+	const hoursDegrees = ((hours / 12) * 360) + 90;
 	hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
 
 
-	console.log(hours + ":" + minutes  + ":" + seconds);
+	// console.log(now);
 }
 
 setInterval(setDate, 1000);
